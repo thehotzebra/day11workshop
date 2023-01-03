d755eae3a90e1c99395a210e3ee10c793987b167
teo_hong_zhang <thehotzebra@gmail.com>
2023-01-03 11:57:09 +0800

    first push

---
+++ demo/demo/src/test/java/com/example/demo/day11workshop/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.day11workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
